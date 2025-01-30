5eb87626f86b9c9e64e3ba2864320468eda68f7d
inversemicron <inversemicron@gmail.com>
2025-01-30 19:31:51 +0000

    fixed library compilation

--- include/ezekiel.h
+++ include/ezekiel.h
@@ -8,15 +8,18 @@
   #define EZK_INCLUDES
 #elif defined(EZK_SO_BUILD)
   #define EZKAPI __attribute__((visibility("default")))
+#elif defined(EZK_USE_SO)
+  #define EZKAPI extern
+  #define EZK_INCLUDES
 #else
+  #define EZKAPI
   #define EZK_INCLUDES
 #endif
 
-#if !defined(EZK_INCLUDES) // we are compiling the library in place or to a library file
-  #include "../src/ezk_window.h"
-#else // we are using symbols and loading from a library file
-  #define EZKAPI
+#if defined(EZK_INCLUDES) // we are compiling the library in place or to a library file
   #include "ezk_window.h"
+#else // we are using symbols and loading from a library file
+  #include "../src/ezk_window.h"
 #endif // EZKAPI
 
 static const int make_iso_compilers_happy;
