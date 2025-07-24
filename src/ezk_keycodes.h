e34bce340cc1427559e5cebfbad117e9d66e5b3c
inversemicron <inversemicron@gmail.com>
2025-07-24 20:32:15 +0100

    Added Win32 HW key mappings.

--- src/ezk_keycodes.h
+++ src/ezk_keycodes.h
@@ -7,7 +7,11 @@
 // input, and just other more ergonomic layouts. 
 
 #include "ezk_primitives.h"
+#include "ezk_platform.h"
 
+#ifdef EZK_WINDOWS
+  #include <windows.h>
+#endif
 
 typedef enum {
 EZK_MOD_SHIFT    = 1 << 0,
@@ -488,4 +492,134 @@ ezk_key ezk_key_ezk_to_x11(int keycode)  {
 ezk_key ezk_key_x11_to_ezk(int keycode)  {
   if (keycode < 0 || keycode >= EZK_X11_KEY_LT_LENGTH) return EZK_KEY_NULL;
   return x11_ezk_lt[keycode];
+}
+
+#define EZK_WIN32_KC_LOOKUP \
+  EZK_WIN32_LT_XM(SPACE,         VK_SPACE) \
+  EZK_WIN32_LT_XM(HASH,          VK_OEM_3) \
+  EZK_WIN32_LT_XM(SINGLEQUOTE,   VK_OEM_7) \
+  EZK_WIN32_LT_XM(COMMA,         VK_OEM_COMMA) \
+  EZK_WIN32_LT_XM(HYPHEN,        VK_OEM_MINUS) \
+  EZK_WIN32_LT_XM(PERIOD,        VK_OEM_PERIOD) \
+  EZK_WIN32_LT_XM(SLASH,         VK_OEM_2) \
+  EZK_WIN32_LT_XM(ZERO,          '0') \
+  EZK_WIN32_LT_XM(ONE,           '1') \
+  EZK_WIN32_LT_XM(TWO,           '2') \
+  EZK_WIN32_LT_XM(THREE,         '3') \
+  EZK_WIN32_LT_XM(FOUR,          '4') \
+  EZK_WIN32_LT_XM(FIVE,          '5') \
+  EZK_WIN32_LT_XM(SIX,           '6') \
+  EZK_WIN32_LT_XM(SEVEN,         '7') \
+  EZK_WIN32_LT_XM(EIGHT,         '8') \
+  EZK_WIN32_LT_XM(NINE,          '9') \
+  EZK_WIN32_LT_XM(SEMICOLON,     VK_OEM_1) \
+  EZK_WIN32_LT_XM(EQUALS,        VK_OEM_PLUS) \
+  EZK_WIN32_LT_XM(OPENBRACKETSQ, VK_OEM_4) \
+  EZK_WIN32_LT_XM(BACKSLASH,     VK_OEM_5) \
+  EZK_WIN32_LT_XM(CLOSEBRACKETSQ, VK_OEM_6) \
+  EZK_WIN32_LT_XM(GRAVE,         VK_OEM_3) \
+  EZK_WIN32_LT_XM(a,             'A') \
+  EZK_WIN32_LT_XM(b,             'B') \
+  EZK_WIN32_LT_XM(c,             'C') \
+  EZK_WIN32_LT_XM(d,             'D') \
+  EZK_WIN32_LT_XM(e,             'E') \
+  EZK_WIN32_LT_XM(f,             'F') \
+  EZK_WIN32_LT_XM(g,             'G') \
+  EZK_WIN32_LT_XM(h,             'H') \
+  EZK_WIN32_LT_XM(i,             'I') \
+  EZK_WIN32_LT_XM(j,             'J') \
+  EZK_WIN32_LT_XM(k,             'K') \
+  EZK_WIN32_LT_XM(l,             'L') \
+  EZK_WIN32_LT_XM(m,             'M') \
+  EZK_WIN32_LT_XM(n,             'N') \
+  EZK_WIN32_LT_XM(o,             'O') \
+  EZK_WIN32_LT_XM(p,             'P') \
+  EZK_WIN32_LT_XM(q,             'Q') \
+  EZK_WIN32_LT_XM(r,             'R') \
+  EZK_WIN32_LT_XM(s,             'S') \
+  EZK_WIN32_LT_XM(t,             'T') \
+  EZK_WIN32_LT_XM(u,             'U') \
+  EZK_WIN32_LT_XM(v,             'V') \
+  EZK_WIN32_LT_XM(w,             'W') \
+  EZK_WIN32_LT_XM(x,             'X') \
+  EZK_WIN32_LT_XM(y,             'Y') \
+  EZK_WIN32_LT_XM(z,             'Z') \
+  EZK_WIN32_LT_XM(DELETE,        VK_DELETE) \
+  EZK_WIN32_LT_XM(SHIFT,         VK_SHIFT) \
+  EZK_WIN32_LT_XM(CONTROL,       VK_CONTROL) \
+  EZK_WIN32_LT_XM(ALT,           VK_MENU) \
+  EZK_WIN32_LT_XM(ALTGR,         VK_RMENU) \
+  EZK_WIN32_LT_XM(ESCAPE,        VK_ESCAPE) \
+  EZK_WIN32_LT_XM(PRINTSCREEN,   VK_SNAPSHOT) \
+  EZK_WIN32_LT_XM(CAPSLOCK,      VK_CAPITAL) \
+  EZK_WIN32_LT_XM(TAB,           VK_TAB) \
+  EZK_WIN32_LT_XM(NUMLOCK,       VK_NUMLOCK) \
+  EZK_WIN32_LT_XM(SCROLLLOCK,    VK_SCROLL) \
+  EZK_WIN32_LT_XM(PAGEUP,        VK_PRIOR) \
+  EZK_WIN32_LT_XM(PAGEDOWN,      VK_NEXT) \
+  EZK_WIN32_LT_XM(INSERT,        VK_INSERT) \
+  EZK_WIN32_LT_XM(HOME,          VK_HOME) \
+  EZK_WIN32_LT_XM(END,           VK_END) \
+  EZK_WIN32_LT_XM(ENTER,         VK_RETURN) \
+  EZK_WIN32_LT_XM(BACKSPACE,     VK_BACK) \
+  EZK_WIN32_LT_XM(PAUSEBREAK,    VK_PAUSE) \
+  EZK_WIN32_LT_XM(MENU,          VK_APPS) \
+  EZK_WIN32_LT_XM(NUMPAD0,       VK_NUMPAD0) \
+  EZK_WIN32_LT_XM(NUMPAD1,       VK_NUMPAD1) \
+  EZK_WIN32_LT_XM(NUMPAD2,       VK_NUMPAD2) \
+  EZK_WIN32_LT_XM(NUMPAD3,       VK_NUMPAD3) \
+  EZK_WIN32_LT_XM(NUMPAD4,       VK_NUMPAD4) \
+  EZK_WIN32_LT_XM(NUMPAD5,       VK_NUMPAD5) \
+  EZK_WIN32_LT_XM(NUMPAD6,       VK_NUMPAD6) \
+  EZK_WIN32_LT_XM(NUMPAD7,       VK_NUMPAD7) \
+  EZK_WIN32_LT_XM(NUMPAD8,       VK_NUMPAD8) \
+  EZK_WIN32_LT_XM(NUMPAD9,       VK_NUMPAD9) \
+  EZK_WIN32_LT_XM(NUMPADPOINT,   VK_DECIMAL) \
+  EZK_WIN32_LT_XM(NUMPADDIVIDE,  VK_DIVIDE) \
+  EZK_WIN32_LT_XM(NUMPADMULTIPLY, VK_MULTIPLY) \
+  EZK_WIN32_LT_XM(NUMPADSUBTRACT, VK_SUBTRACT) \
+  EZK_WIN32_LT_XM(NUMPADPLUS,    VK_ADD) \
+  EZK_WIN32_LT_XM(NUMPADENTER,   VK_RETURN) \
+  EZK_WIN32_LT_XM(ARROWUP,       VK_UP) \
+  EZK_WIN32_LT_XM(ARROWDOWN,     VK_DOWN) \
+  EZK_WIN32_LT_XM(ARROWLEFT,     VK_LEFT) \
+  EZK_WIN32_LT_XM(ARROWRIGHT,    VK_RIGHT) \
+  EZK_WIN32_LT_XM(F1,            VK_F1) \
+  EZK_WIN32_LT_XM(F2,            VK_F2) \
+  EZK_WIN32_LT_XM(F3,            VK_F3) \
+  EZK_WIN32_LT_XM(F4,            VK_F4) \
+  EZK_WIN32_LT_XM(F5,            VK_F5) \
+  EZK_WIN32_LT_XM(F6,            VK_F6) \
+  EZK_WIN32_LT_XM(F7,            VK_F7) \
+  EZK_WIN32_LT_XM(F8,            VK_F8) \
+  EZK_WIN32_LT_XM(F9,            VK_F9) \
+  EZK_WIN32_LT_XM(F10,           VK_F10) \
+  EZK_WIN32_LT_XM(F11,           VK_F11) \
+  EZK_WIN32_LT_XM(F12,           VK_F12)
+
+#define EZK_WIN32_KEY_LT_LENGTH 253 // needs to be updated if more keys are added
+
+#define EZK_WIN32_LT_XM(k,i) [i] = EZK_KEY_##k,
+
+static const ezk_key win32_ezk_lt[EZK_WIN32_KEY_LT_LENGTH] = {
+  EZK_WIN32_KC_LOOKUP
+};
+
+#undef EZK_WIN32_LT_XM
+#define EZK_WIN32_LT_XM(k,i) [EZK_KEY_##k] = i,
+
+static const ezk_key ezk_win32_lt[EZK_WIN32_KEY_LT_LENGTH] = {
+  EZK_WIN32_KC_LOOKUP
+};
+
+#undef EZK_WIN32_LT_XM
+
+ezk_key ezk_key_ezk_to_win32(int keycode)  {
+  if (keycode < 0 || keycode >= EZK_WIN32_KEY_LT_LENGTH) return EZK_KEY_NULL;
+  return ezk_win32_lt[keycode];
+}
+
+ezk_key ezk_key_win32_to_ezk(int keycode)  {
+  if (keycode < 0 || keycode >= EZK_WIN32_KEY_LT_LENGTH) return EZK_KEY_NULL;
+  return win32_ezk_lt[keycode];
 }
