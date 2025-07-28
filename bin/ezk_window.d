911134597b3a19bca22a495c06e5d2465f10c77a
inversemicron <inversemicron@gmail.com>
2025-07-28 22:36:57 +0100

    Removed binaries.

--- bin/ezk_window.d
+++
@@ -1,5 +0,0 @@
-bin/ezk_window.o: src/ezk_window.c src/ezk_api.h src/ezk_platform.h \
- src/ezk_window/ezk_window.h src/ezk_window/../ezk_keycodes.h \
- src/ezk_window/../ezk_primitives.h src/ezk_window/../ezk_platform.h \
- src/ezk_window/../ezk_primitives.h src/ezk_window/../ezk_callback.h \
- src/./ezk_window/x11.h src/./ezk_window/./ezk_window.h
